6214f9713a0c4e8f9ba4a5e862d4d6b5352b4cfa
Hegert Taresalu <herkuti@gmail.com>
2022-04-19 10:22:43 +0300

    finish

--- app/src/main/java/com/example/lab8foreground/MainActivity.java
+++ app/src/main/java/com/example/lab8foreground/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.lab8foreground;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.ActivityManager;
+import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -19,14 +24,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ConstraintLayout contained = findViewById(R.id.myConstraintlayout);
+
         if (isMyServiceRunning(ForegroundService.class)){
             findViewById(R.id.button).setEnabled(false);
             Snackbar snackbar = Snackbar.make(contained,R.string.stop,Snackbar.LENGTH_INDEFINITE).setAction("Stop",view -> stopMyService());
+            snackbar.show();
 
         }
         else {
             Log.i("MainActivity", "Foreground Service not running");
         }
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        boolean areNotificationEnabled = notificationManager.areNotificationsEnabled();
+        if (!areNotificationEnabled){
+            Snackbar snackbar = Snackbar.make(contained, R.string.enabled, Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Enable", view -> openNotificationSettingsForApp());
+            snackbar.show();
+        }
+
+        findViewById(R.id.button).setOnClickListener(view -> {
+            EditText input = view.findViewById(R.id.editTextTextPersonName);
+            String text = input.getText().toString();
+            Intent foregroundStart = new Intent(this,ForegroundService.class);
+            foregroundStart.putExtra("input",text);
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O){
+                startForegroundService(foregroundStart);
+
+            }
+            else {
+                startService(foregroundStart);
+            }
+        });
+
     }
 
     private void stopMyService() {
@@ -43,5 +73,13 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return false;
+    }
+    private void openNotificationSettingsForApp(){
+        Intent intent = new Intent();
+        intent.setAction("android,settings.APP_NOTIFICATION_SETTINGS");
+        intent.putExtra("app_package",getPackageName());
+        intent.putExtra("app_uid",getApplicationInfo().uid);
+        intent.putExtra("android.provider.extra.APP_PACKAGE",getPackageName());
+        startActivity(intent);
     }
 }
